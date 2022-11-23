86170f347052a30c53cf89c757fb5765d516e7ce
Mattsun5 <sundaymatt703@gmail.com>
2022-11-23 12:13:21 -0800

    printf initial task

--- functions1.c
+++ functions1.c
@@ -49,26 +49,26 @@ int print_unsigned(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 int print_octal(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 
-int i = BUFF_SIZE - 2;
-unsigned long int num = va_arg(types, unsigned long int);
-unsigned long int init_num = num;
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
 
-UNUSED(width);
+	UNUSED(width);
 
-num = convert_size_unsgnd(num, size);
+	num = convert_size_unsgnd(num, size);
 
-if (num == 0)
-buffer[i--] = '0';
+	if (num == 0)
+		buffer[i--] = '0';
 
-buffer[BUFF_SIZE - 1] = '\0';
+	buffer[BUFF_SIZE - 1] = '\0';
 
-while (num > 0)
-{
-buffer[i--] = (num % 8) + '0';
-num /= 8;
+	while (num > 0)
+	{
+		buffer[i--] = (num % 8) + '0';
+		num /= 8;
 }
 
 if (flags & F_HASH && init_num != 0)
